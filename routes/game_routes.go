c5bac25fe7c3aa8cd77f59a7409babd3f12752ed
UniTofu <mrtyoukin@gmail.com>
2021-09-27 03:13:35 +0900

    refactor: 定義したAPIはroutesフォルダに移動

---
+++ routes/game_routes.go
@@ -0,0 +1,48 @@
+package routes
+
+import (
+	"log"
+	"nam-club/NumBuyer_back/consts"
+	"nam-club/NumBuyer_back/services/logic"
+	"nam-club/NumBuyer_back/services/utils"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func RoutesGame(server *socketio.Server) {
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		s.LeaveAll()
+		log.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", consts.ToServerJoinQuickMatch, func(s socketio.Conn, msg string) {
+		// 一つの部屋にのみ入室した状態にする
+		s.LeaveAll()
+		s.Join(msg)
+
+		u := logic.CreateNewPlayer("JUNPEI", "rid", false)
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.ToResponseFormat(u))
+	})
+
+	server.OnEvent("/", consts.ToServerJoinFriendMatch, func(s socketio.Conn, msg string) {
+		// 一つの部屋にのみ入室した状態にする
+		s.LeaveAll()
+		s.Join(msg)
+
+		u := logic.CreateNewPlayer("FRIEND", "JUNPEI", false)
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.ToResponseFormat(u))
+	})
+
+	server.OnEvent("/", consts.ToServerCreateMatch, func(s socketio.Conn, msg string) {
+		log.Println("creatematch: ", msg)
+
+		g := logic.CreateNewGame("OWENER")
+		s.LeaveAll()
+		s.Join(g.RoomID)
+
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.ToResponseFormat(g))
+	})
+}
