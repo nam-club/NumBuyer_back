506fcf5d2f8bd07aad7a0dedcdbc3fcfe7a572f8
UniTofu <mrtyoukin@gmail.com>
2021-10-19 05:34:54 +0900

    refactor: 関数の移動と命名の変更

--- routes/game_routes.go
+++ routes/game_routes.go
@@ -2,15 +2,14 @@ package routes
 
 import (
 	"encoding/json"
-	"log"
 	"nam-club/NumBuyer_back/consts"
 	"nam-club/NumBuyer_back/models/orgerrors"
 	"nam-club/NumBuyer_back/models/requests"
 	"nam-club/NumBuyer_back/models/responses"
 	"nam-club/NumBuyer_back/services/logic"
+	"nam-club/NumBuyer_back/utils"
 
 	socketio "github.com/googollee/go-socket.io"
-	"github.com/pkg/errors"
 	"gopkg.in/go-playground/validator.v9"
 )
 
@@ -23,29 +22,29 @@ func RoutesGame(server *socketio.Server) {
 
 		req := &requests.JoinQuickMatch{}
 		if e := valid(msg, req); e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 
 		roomId, e := logic.GetRandomRoomId()
 		if e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 
 		if roomId == "" {
-			s.Emit(consts.FromServerGameJoin, responseError(orgerrors.NewGameNotFoundError("")))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(orgerrors.NewGameNotFoundError("")))
 			return
 		}
 
 		player, e := logic.CreateNewPlayer(req.PlayerName, roomId, false)
 		if e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 
 		resp := responses.JoinResponse{RoomID: roomId, PlayerID: player.PlayerID}
-		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, response(resp))
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.Response(resp))
 	})
 
 	server.OnEvent("/", consts.ToServerJoinFriendMatch, func(s socketio.Conn, msg string) {
@@ -55,62 +54,62 @@ func RoutesGame(server *socketio.Server) {
 
 		req := &requests.JoinFriendMatch{}
 		if e := valid(msg, req); e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 
 		player, e := logic.CreateNewPlayer(req.PlayerName, req.RoomID, false)
 		if e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 		resp := responses.JoinResponse{RoomID: req.RoomID, PlayerID: player.PlayerID}
-		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, response(resp))
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.Response(resp))
 	})
 
 	server.OnEvent("/", consts.ToServerCreateMatch, func(s socketio.Conn, msg string) {
 		req := &requests.CreateMatch{}
 		if e := valid(msg, req); e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 
 		resp, e := logic.CreateNewGame(req.PlayerName)
 		if e != nil {
-			s.Emit(consts.FromServerGameJoin, responseError(e))
+			s.Emit(consts.FromServerGameJoin, utils.ResponseError(e))
 			return
 		}
 		s.LeaveAll()
 		s.Join(resp.RoomID)
 
-		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, response(resp))
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameJoin, utils.Response(resp))
 	})
 
 	server.OnEvent("/", consts.ToServerGamePlayersInfo, func(s socketio.Conn, msg string) {
 		req := &requests.GamePlayerInfo{}
 		if e := valid(msg, req); e != nil {
-			s.Emit(consts.FromServerGamePlayersInfo, responseError(e))
+			s.Emit(consts.FromServerGamePlayersInfo, utils.ResponseError(e))
 			return
 		}
 		resp, e := logic.GetPlayersInfo(req.RoomID, req.PlayerID)
 		if e != nil {
-			s.Emit(consts.FromServerGamePlayersInfo, responseError(e))
+			s.Emit(consts.FromServerGamePlayersInfo, utils.ResponseError(e))
 		}
-		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGamePlayersInfo, response(resp))
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGamePlayersInfo, utils.Response(resp))
 	})
 
 	server.OnEvent("/", consts.ToServerGameNextTurn, func(s socketio.Conn, msg string) {
 		req := &requests.GameNextTurn{}
 		if e := valid(msg, req); e != nil {
-			s.Emit(consts.FromServerGameNextTurn, responseError(e))
+			s.Emit(consts.FromServerGameNextTurn, utils.ResponseError(e))
 			return
 		}
 		resp, e := logic.NextTurn(req.RoomID, req.PlayerID)
 		if e != nil {
-			s.Emit(consts.FromServerGameNextTurn, responseError(e))
+			s.Emit(consts.FromServerGameNextTurn, utils.ResponseError(e))
 			return
 		}
-		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameNextTurn, response(resp))
+		server.BroadcastToRoom("/", s.Rooms()[0], consts.FromServerGameNextTurn, utils.Response(resp))
 	})
 }
 
@@ -126,31 +125,4 @@ func valid(reqBody string, result interface{}) error {
 	}
 
 	return nil
-}
-
-// インスタンスをレスポンス形式（JSON文字列）に変換する
-func response(val interface{}) string {
-	retJson, _ := json.Marshal(val)
-	return string(retJson)
-}
-
-// インスタンスをレスポンス形式（JSON文字列）に変換する
-func responseError(err error) string {
-	errUnwrap := errors.Unwrap(err)
-	var retJson []byte
-	switch e := errUnwrap.(type) {
-	case *orgerrors.ValidationError, *orgerrors.GameNotFoundError:
-		retJson, _ = json.Marshal(e)
-		break
-	case *orgerrors.InternalServerError:
-		log.Printf("[ERROR] %+v\n", e)
-		retJson, _ = json.Marshal(e)
-		break
-	default:
-		log.Printf("[ERROR] %+v\n", err)
-		retJson, _ = json.Marshal(errors.Unwrap(orgerrors.NewInternalServerError("")))
-		break
-	}
-
-	return string(retJson)
 }
---
+++ utils/response.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"nam-club/NumBuyer_back/models/orgerrors"
+
+	"github.com/pkg/errors"
+)
+
+// インスタンスをレスポンス形式（JSON文字列）に変換する
+func Response(val interface{}) string {
+	retJson, _ := json.Marshal(val)
+	return string(retJson)
+}
+
+// インスタンスをレスポンス形式（JSON文字列）に変換する
+func ResponseError(err error) string {
+	errUnwrap := errors.Unwrap(err)
+	var retJson []byte
+	switch e := errUnwrap.(type) {
+	case *orgerrors.ValidationError, *orgerrors.GameNotFoundError:
+		retJson, _ = json.Marshal(e)
+		break
+	case *orgerrors.InternalServerError:
+		log.Printf("[ERROR] %+v\n", e)
+		retJson, _ = json.Marshal(e)
+		break
+	default:
+		log.Printf("[ERROR] %+v\n", err)
+		retJson, _ = json.Marshal(errors.Unwrap(orgerrors.NewInternalServerError("")))
+		break
+	}
+
+	return string(retJson)
+}
