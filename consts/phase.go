4286a4be2def46a4780d24a13e28276459c0724e
UniTofu <mrtyoukin@gmail.com>
2021-10-24 14:57:09 +0900

    wip

--- consts/phase.go
+++ consts/phase.go
@@ -5,16 +5,18 @@ import "nam-club/NumBuyer_back/models/orgerrors"
 type Phase string
 
 const (
-	PhaseBeforeStart = Phase("BEFORE_START")
-	PhaseAuction     = Phase("AUCTION")
-	PhaseCalculate   = Phase("CALCULATE")
-	PhaseResult      = Phase("RESULT")
-	PhaseEnd         = Phase("END")
+	PhaseBeforeStart     = Phase("BEFORE_START")
+	PhaseBeforeAuction   = Phase("BEFORE_AUCTION")
+	PhaseAuction         = Phase("AUCTION")
+	PhaseAuctionResult   = Phase("AUCTION_RESULT")
+	PhaseCalculate       = Phase("CALCULATE")
+	PhaseCalculateResult = Phase("CALCULATE_RESULT")
+	PhaseEnd             = Phase("END")
 )
 
 func (v Phase) Valid() error {
 	switch v {
-	case PhaseBeforeStart, PhaseAuction, PhaseCalculate, PhaseResult, PhaseEnd:
+	case PhaseBeforeStart, PhaseBeforeAuction, PhaseAuction, PhaseCalculate, PhaseCalculateResult, PhaseEnd:
 		return nil
 	default:
 		return orgerrors.NewInternalServerError("invalid phase type")
--- consts/phase_time.go
+++ consts/phase_time.go
@@ -2,11 +2,13 @@ package consts
 
 const (
 	// 各フェーズの持ち時間（秒）
-	PhaseTimeBeforeStart = PhaseTimeValueInfinite
-	PhaseTimeAuction     = 60
-	PhaseTimeCalculate   = 60
-	PhaseTimeResult      = 10
-	PhasetimeEnd         = PhaseTimeValueInfinite
+	PhaseTimeBeforeStart     = PhaseTimeValueInfinite
+	PhaseTimeBeforeAuction   = 14
+	PhaseTimeAuction         = 30
+	PhaseTimeAuctionResult   = 5
+	PhaseTimeCalculate       = 20
+	PhaseTimeCalculateResult = 5
+	PhaseTimeEnd             = PhaseTimeValueInfinite
 
 	// -1なら無制限
 	PhaseTimeValueInfinite = -1
