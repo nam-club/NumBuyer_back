666b9125e786d74351ee642583452acd8ca91dc6
UniTofu <mrtyoukin@gmail.com>
2021-11-21 14:19:57 +0900

    fix: bidでコイン数未指定時にバリデーションエラーになってしまうため修正

--- models/requests/game_bid.go
+++ models/requests/game_bid.go
@@ -3,6 +3,6 @@ package requests
 type GameBid struct {
 	RoomID   string `json:"roomId" validate:"required,min=8,max=16"`
 	PlayerID string `json:"playerId" validate:"required"`
-	Coin     int    `json:"coin" validate:"min=1,max=1000000"`
+	Coin     int    `json:"coin" validate:"min=0,max=1000000"`
 	Action   string `json:"action" validate:"required"`
 }
